ed88020a6ba35bfa2fe253967a35b13cd4373515
Leafarneo <leafarneo@gmail.com>
2017-08-14 22:57:32 -0300

    Initial commit

---
+++ Entities/Employment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Entities
+{
+    public class Employment
+    {
+        public Person Employee { get; set; }
+
+        public DateTime EmploymentStartDate { get; set; }
+
+        public DateTime? EmploymentEndDate { get; set; }
+
+        /// <summary>
+        ///     Return the number of Years that a employee has been Employed
+        /// </summary>
+        /// <returns></returns>
+        public double NumberOfYearsEmployed
+        {
+            get { return 0; }
+        }
+    }
+}
