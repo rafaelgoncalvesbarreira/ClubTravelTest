64443e64587ee073b67c15c80d0799b5bc32d853
Leafarneo <leafarneo@gmail.com>
2017-08-15 23:19:24 -0300

    adding Web Api project

---
+++ WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,42 @@
+﻿using Data;
+using Data.Interfaces;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using WebApi.App_Start;
+
+namespace WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            var container = CreateUnityContainer();
+            config.DependencyResolver = new MockInjection(container);
+        }
+
+        private static UnityContainer CreateUnityContainer()
+        {
+            var container = new UnityContainer();
+            container.RegisterType<ITestRepository, TestRepository>(new ContainerControlledLifetimeManager());
+
+            return container;
+        }
+    }
+}
---
+++ WebApi/Controllers/CompanyController.cs
@@ -0,0 +1,48 @@
+﻿using Data.Interfaces;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class CompanyController : ApiController
+    {
+        private ITestRepository repository;
+
+        public CompanyController(ITestRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        // GET: api/Company
+        public IEnumerable<Company> Get()
+        {
+            return repository.FindAllCompanies();
+        }
+
+        // GET: api/Company/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Company
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Company/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Company/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
