3eb5fe7bba6f4f2f272345e3ed753eee5a284220
Leafarneo <leafarneo@gmail.com>
2017-08-15 21:46:50 -0300

    wrong test project and counting all companies

---
+++ XUnitTests/Queries.cs
@@ -0,0 +1,21 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTests
+{
+    public class Queries
+    {
+        [Fact]
+        public void CountAllCompanies()
+        {
+            var repository = new TestRepository();
+            var count = repository.FindAllCompanies().Count;
+            Assert.True(count == 7);
+        }
+    }
+}
